a7bcf24ee2cdae6654633351256950ad34b7df60
Karlosh Yadav <164324033+karloshyadav@users.noreply.github.com>
2026-01-11 16:57:40 +0545

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const RESUME_URL =
   process.env.NEXT_PUBLIC_RESUME_URL ??
-  "/assets/cvv.pdf";
+  "public/assets/Karlosh_Yadav_Resume.pdf";
 
 export default function Hero() {
   return (
@@ -34,7 +34,7 @@ export default function Hero() {
 
           <div className="flex flex-col md:flex-row gap-4 md:gap-6 items-center md:items-start lg:mx-0 mx-4">
             <Link
-              href={public/assets/Karlosh_Yadav_Resume.pdf}
+              href={RESUME_URL}
               target="_blank"
               rel="noopener noreferrer"
               className="download-button button inline-flex items-center justify-center text-center"
